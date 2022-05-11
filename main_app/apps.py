e78c3f7a916389d1ee5b73bea42af6b9a293d3ae
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-05-11 06:25:50 +0400

    Update apps.py

--- main_app/apps.py
+++ main_app/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import redis
 
 class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -6,3 +7,7 @@ class MainAppConfig(AppConfig):
 
     def ready(self):
         import main_app.signals
+red = redis.Redis(
+    host='redis-17188.c8.us-east-1-2.ec2.cloud.redislabs.com',
+    port=17188,
+    password='jmPxy4TYtYlmyOzdt0B1EBbYU8pOKEPm'
